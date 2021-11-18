56a0cf577cab45ff60751e63ea7c1975cd302011
Carlos Aparicio <apariciovivero>
2021-11-18 17:08:00 -0500

    actualizacion urls

--- django_init/urls.py
+++ django_init/urls.py
@@ -13,9 +13,32 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
+from aplicaciones import apiinvoice
+from aplicaciones.api.views import InvoiceList
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Back-End Emprendedor API",
+        default_version='v1',
+        description="Lógica de negocio de sitio de emprendedores",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="carlos.apariciov@ug.edu.ec"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
+    path('api/invoices/', InvoiceList.as_view(), name='invoice_list'),
 ]
