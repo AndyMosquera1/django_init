9294eff247e0bb32bbecdb876583007e52ab6b02
amosquera <andy.mosqueral@outlook.es>
2021-11-18 23:47:33 -0500

    Creacion de Serializadores para los modelos faltantes

--- aplicaciones/api/serializers.py
+++ aplicaciones/api/serializers.py
@@ -1,20 +1,41 @@
 from rest_framework import serializers
 
-from aplicaciones.facturacion.models import Invoice, Customer
-
-
-
-class InvoiceSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Invoice
-        fields = ['createtime','customer']
-
-        def __str__(self):
-            return self.first_name
+from aplicaciones.facturacion.models import Invoice, Customer, Items, Sku, Brand, Category
 
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
+        fields = '__all__'
+
+class InvoiceSerializer(serializers.ModelSerializer):
+    customer = CustomerSerializer()
+
+    class Meta:
+        model = Invoice
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+class SkuSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    brand  = BrandSerializer()
+
+    class Meta:
+        model = Sku
+        fields = '__all__'
+
+class ItemsSerializer(serializers.ModelSerializer):
+    sku = SkuSerializer()
+
+    class Meta:
+        model = Items
         fields = '__all__'
