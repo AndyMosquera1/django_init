b91230a3909876d986cd386b34e3064546799e85
amosquera <andy.mosqueral@outlook.es>
2021-11-18 23:48:39 -0500

    modificacion views

--- aplicaciones/api/views.py
+++ aplicaciones/api/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 from django.db.models import Q
 
-from aplicaciones.api.serializers import InvoiceSerializer
-from aplicaciones.facturacion.models import Invoice
+from aplicaciones.api.serializers import InvoiceSerializer, CustomerSerializer, ItemsSerializer
+from aplicaciones.facturacion.models import Invoice, Customer, Items
 
 
-class InvoiceList(APIView):
-    def get(self, request):
-        prod = Invoice.objects.all()
-        data = InvoiceSerializer(prod, many=True).data
-        print (data)
-        return Response(data)
+class Invoiceapi(viewsets.ModelViewSet):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
+
+
+class Customerapi(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class Itemsapi(viewsets.ModelViewSet):
+    queryset = Items.objects.all()
+    serializer_class = ItemsSerializer
