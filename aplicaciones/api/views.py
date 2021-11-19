49f07ac1dbff26279c970c46a9f925ba6fa6b633
amosquera <andy.mosqueral@outlook.es>
2021-11-19 09:33:19 -0500

    metodo api view consultar facturas y items

--- aplicaciones/api/views.py
+++ aplicaciones/api/views.py
@@ -1,5 +1,9 @@
+from django.core import serializers
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.utils import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
@@ -10,9 +14,31 @@ from aplicaciones.api.serializers import InvoiceSerializer, CustomerSerializer, 
 from aplicaciones.facturacion.models import Invoice, Customer, Items
 
 
+@api_view(['GET'])
+def api_invoice(request):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer(queryset, many=True)
+    dicc = {'id': '','createtime': '','document_number': '','customer':'', 'items':'' }
+
+    for x in queryset:
+        queryset_invoice = Invoice.objects.filter(id=x.id)
+        dicc['id']=x.id
+        dicc['createtime']=x.createtime
+        dicc['customer']= SerializaCustomer(x.customer_id)
+        queryset_item = Items.objects.filter(invoice_id=x.id)
+        dicc['items'] = ItemsSerializer(queryset_item, many=True).data
+
+
+    #dicc = {'api':serializer_class.data}
+    return Response(dicc)
+
+def SerializaCustomer(id_customer):
+    response = CustomerSerializer(Customer.objects.filter(id=id_customer),many=True).data[0]
+    return response
 class Invoiceapi(viewsets.ModelViewSet):
     queryset = Invoice.objects.all()
     serializer_class = InvoiceSerializer
+
 
 
 class Customerapi(viewsets.ModelViewSet):
